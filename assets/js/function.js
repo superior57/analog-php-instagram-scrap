c9906d4d1a185ac0c9716f7f756785bf3d8b7e6a
talentedexpert0057 <petpanda0057@gmail.com>
2020-11-22 01:31:38 +1000

    functions...

--- assets/js/function.js
+++ assets/js/function.js
@@ -2,38 +2,44 @@ var index = 1;
 setInterval(() => {
     
     if($('#step_5').css('display') == "block") {
-        $('#advise').css('display', 'block');
-
         var data = demoDatas.split('</li><li');
-        if(index == data.length - 1) {
-            $('#advise').css('display', 'none');
-            $('#commentsLoaderAnimation').css("display", "block");
-            $('#comments_user').css("display", "none");  
-            // $('#comments_user').empty();  
-            clearInterval();
-            index = 0;
-        }     
+        var element = `<li${data[index]}</li><li${data[index+1]}</li><li${data[index+2]}</li>`;
+            
         if(index == 1) {
-            $('#comments_user').empty(); 
-        }
-
-        $('#commentsLoaderAnimation').css("display", "none");
-        $('#comments_user').css("display", "block");    
+            init(data);
+        } 
+        if(index == data.length - 1) {
+            desctructure();
+        } 
         
-        $('#comments_user').append(`<li
-        ${
-            data[index]
-        }</li>
-        `); 
-        // alert(demoDatas.split('</li><li')[3]);
-        index ++;        
+        
+        $('#comments_user').append(element); 
+        
+        index = index + 3;        
         scrollBottom($('#comments_user'));
     }    
     
-}, 10);
+}, 500);
 
 function scrollBottom(element) {
     var wtf    = element;
     var height = wtf[0].scrollHeight;
     wtf.scrollTop(height);
+}
+
+function init(data) {
+    alert(data.length);
+    $('#advise').css('display', 'block');  
+    $('#comments_user').empty(); 
+    $('#commentsLoaderAnimation').css("display", "none");
+    $('#comments_user').css("display", "block");
+}
+
+function desctructure() {
+    $('#advise').css('display', 'none');
+    $('#commentsLoaderAnimation').css("display", "block");
+    $('#comments_user').css("display", "none");  
+    // $('#comments_user').empty();  
+    clearInterval();
+    index = 0;
 }
