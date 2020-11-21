9503292dc23096a427d25a3b9d34ba8a04df331c
talentedexpert0057 <petpanda0057@gmail.com>
2020-11-22 01:20:33 +1000

    changed

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,13 +1,22 @@
 var index = 1;
-
 setInterval(() => {
     
     if($('#step_5').css('display') == "block") {
+        $('#advise').css('display', 'block');
+
         var data = demoDatas.split('</li><li');
         if(index == data.length - 1) {
+            $('#advise').css('display', 'none');
+            $('#commentsLoaderAnimation').css("display", "block");
+            $('#comments_user').css("display", "none");  
+            // $('#comments_user').empty();  
             clearInterval();
             index = 0;
         }     
+        if(index == 1) {
+            $('#comments_user').empty(); 
+        }
+
         $('#commentsLoaderAnimation').css("display", "none");
         $('#comments_user').css("display", "block");    
         
@@ -21,7 +30,7 @@ setInterval(() => {
         scrollBottom($('#comments_user'));
     }    
     
-}, 100);
+}, 10);
 
 function scrollBottom(element) {
     var wtf    = element;
