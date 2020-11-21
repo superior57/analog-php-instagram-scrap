356aa1d8baf8e719ab23064591758ccc72458fac
talentedexpert0057 <petpanda0057@gmail.com>
2020-11-22 01:13:30 +1000

    made demo data

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,12 +1,23 @@
-
+var index = 1;
 
 setInterval(() => {
+    
     if($('#step_5').css('display') == "block") {
+        var data = demoDatas.split('</li><li');
+        if(index == data.length - 1) {
+            clearInterval();
+            index = 0;
+        }     
         $('#commentsLoaderAnimation').css("display", "none");
-        $('#comments_user').css("display", "block");
-        $('#comments_user').append(`
-            <li id="comment-673" class="commentList" data-userid="44436640635" data-username="yasmimvaledasilva" data-time="1604861032" data-commentid="17861565395213759" data-pic="https://instagram.frix7-1.fna.fbcdn.net/v/t51.2885-19/s150x150/123218979_995669044232600_8028995411651125731_n.jpg?_nc_ht=instagram.frix7-1.fna.fbcdn.net&amp;_nc_ohc=ICAA05bRdqcAX_mrIfc&amp;tp=1&amp;oh=98616b700e54f1fabb4db759564f6f7b&amp;oe=5FE1AC2E"><span class="userBold" id="username-673">@yasmimvaledasilva</span>: <span id="caption-673">Kkkkkkkkkk Estou Passando Mal Aqui Kkk😂😂😂😂😂😂😂</span></li>
+        $('#comments_user').css("display", "block");    
+        
+        $('#comments_user').append(`<li
+        ${
+            data[index]
+        }</li>
         `); 
+        // alert(demoDatas.split('</li><li')[3]);
+        index ++;        
         scrollBottom($('#comments_user'));
     }    
     
